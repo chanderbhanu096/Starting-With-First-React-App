e82f5abb6387c8fedb853f85fb0195adeebd89ab
Chander Bhanu <chanderbhanu096@gmail.com>
2020-07-27 08:56:25 +0530

    list from Array and assigning Keys with map fxn

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const element = React.createElement('div',null,'hello how are you');
-const list = React.createElement('ol',null,React.createElement('li',null,'Apple'),
-React.createElement('li',null,'Mango'),React.createElement('li',null,'Banana'),)
+// const element = React.createElement('div',null,'hello how are you');
+
+/* const list = React.createElement('ol',null,React.createElement('li',null,'Apple'),
+  React.createElement('li',{id:'Mango'},'Mango'),React.createElement('li',null,'Banana'),)*/
+
+const people = [
+  {name: 'ankush'},
+  {name: 'bhanu'},
+  {name: 'bholu'}
+]
+
+/*const listfromarray = React.createElement('ol',null,
+people.map(person =>React.createElement('li',{key: person.name},person.name)))*/
+
+
 ReactDOM.render(
   // <React.StrictMode>
   //   {/* <App /> */}
 
   // </React.StrictMode>,
 
-  list,
+  listfromarray,
   document.getElementById('root')
 );
 
