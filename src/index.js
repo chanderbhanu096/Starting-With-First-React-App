9b1cce98e360b2ddc62d6e616350992fbe5994c2
Chander Bhanu <chanderbhanu096@gmail.com>
2020-07-27 08:35:44 +0530

    Nesting Components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const element = React.createElement('div',null,'hello how are you')
+const element = React.createElement('div',null,'hello how are you');
+const list = React.createElement('ol',null,React.createElement('li',null,'Apple'),
+React.createElement('li',null,'Mango'),React.createElement('li',null,'Banana'),)
 ReactDOM.render(
   // <React.StrictMode>
   //   {/* <App /> */}
 
   // </React.StrictMode>,
-  element,
+
+  list,
   document.getElementById('root')
 );
 
