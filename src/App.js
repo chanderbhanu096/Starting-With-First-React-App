d512d1f7d617c9653cbd5b9872c6acc2da3477fb
Chander Bhanu <chanderbhanu096@gmail.com>
2020-07-27 21:38:15 +0530

    importing component from other js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends Component {
+  render() {
+    const people = [{ name: "ankush" }, { name: "bhanu" }, { name: "bholu" }];
+
+    return (
+      <ol>
+        {people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList />
+        <ContactList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from './App';
+import * as serviceWorker from "./serviceWorker";
+//import React, { Component } from 'react';
+//class ContactList extends Component {}
+/* Above two lines are the alternate for the
+  class ContactList extends React.Component {}
+  Option for the that option one doesn't need to import*/
 
+// Rendering class/ component
+class ContactList extends React.Component {
+  render() {
+    const people = [{ name: "ankush" }, { name: "bhanu" }, { name: "bholu" }];
+
+    return (
+      <ol>
+        {people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    );
+  }
+}
 // const element = React.createElement('div',null,'hello how are you');
 
 /* const list = React.createElement('ol',null,React.createElement('li',null,'Apple'),
-  React.createElement('li',{id:'Mango'},'Mango'),React.createElement('li',null,'Banana'),)*/
+  React.createElement('li',{id:'Mango'},'Mango'),React.createElement('li',null,'Banana'),) */
 
-const people = [
+/* const people = [
   {name: 'ankush'},
   {name: 'bhanu'},
   {name: 'bholu'}
-]
+] */
 
-/*const listfromarray = React.createElement('ol',null,
-people.map(person =>React.createElement('li',{key: person.name},person.name)))*/
+// Using JSX
+/* const element = <ol>
+  {people.map((person)=>(
+    <li key={person.name}>{person.name}</li>
+  ))}
+</ol> */
 
+/* const listfromarray = React.createElement('ol',null,
+people.map(person =>React.createElement('li',{key: person.name},person.name))) */
 
 ReactDOM.render(
   // <React.StrictMode>
   //   {/* <App /> */}
 
   // </React.StrictMode>,
-
-  listfromarray,
-  document.getElementById('root')
+  
+  <App />,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
